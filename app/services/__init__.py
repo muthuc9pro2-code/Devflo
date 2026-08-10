d5c5d82e639912d80a69c64ba2841f62bf7150c4
Muthu kumar <muthuc9pro2@gmail.com>
2026-08-10 12:18:02 +0000

    removing the identity_resolver(n+1 query architect)

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -6,5 +6,4 @@ from .evidence_store import persist_evidence_batch
 from .persistent_identity_resolver import resolve_evidence_identity
 from .identity_persister import persist_resolved_identities
 from .persistent_timeline import stream_timeline_evidence
-from .identity_reader import stream_resolved_identities
 from .timeline_processor import process_persisted_timelines
